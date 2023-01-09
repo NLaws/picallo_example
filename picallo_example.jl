21eefae6699d358dd3e82f9251e67a6b272b3c4d
Nick Laws <laws.nick@gmail.com>
2023-01-08 19:39:29 -0700

    fixed the epsilon updates, but start from (1,1) still bad?

--- picallo_example.jl
+++ picallo_example.jl
@@ -19,28 +19,29 @@ x2_kp1(x1, x2; τ = 1/4) = x2 - τ * ∇x2_F2(x1, x2) + Sx1x2(x1, x2) * (-τ * D
 x2_kp1_eps(x1, x2; τ = 1/4, ϵ = 1/4) = x2 - τ/ϵ * ∇x2_F2(x1, x2)
 
 function main(x1_0 = 1.0, x2_0 = 1.0, itermax = 40; τ=1/4)
-    x1s = zeros(itermax); x2s = zeros(itermax); x2eps = zeros(itermax)
-    x1s[1] = x1_0; x2s[1] = x2_0; x2eps[1] = x2_0
+    x1s = zeros(itermax); x2s = zeros(itermax); x1eps = zeros(itermax); x2eps = zeros(itermax)
+    x1s[1] = x1_0; x2s[1] = x2_0; x1eps[1] = x1_0; x2eps[1] = x2_0
     for k = 2:itermax
-        x1k = x1s[k-1]; x2k = x2s[k-1]; x2keps = x2eps[k-1]
+        x1k = x1s[k-1]; x2k = x2s[k-1]; x1keps = x1eps[k-1]; x2keps = x2eps[k-1]
         x1s[k] = x1_kp1(x1k, x2k; τ=τ)
         x2s[k] = x2_kp1(x1k, x2k; τ=τ)
-        x2eps[k] = x2_kp1_eps(x1k, x2keps; τ = τ)
+        x1eps[k] = x1_kp1(x1keps, x2keps; τ=τ)
+        x2eps[k] = x2_kp1_eps(x1keps, x2keps; τ = τ)
     end
-    return x1s, x2s, x2eps
+    return x1s, x2s, x1eps, x2eps
 end
 
-x1s, x2s, x2eps = main(0.6,0.6)  # 1,1 does not converge !? nor 0.7,0.7 and higher tenths places
+x1s, x2s, x1eps, x2eps = main(0.6,0.6)  # 1,1 does not converge !? nor 0.7,0.7 and higher tenths places
 plot(x1s, x2s, seriestype = :scatter, label="PS")
-plot!(x1s, x2eps, seriestype = :scatter, label="GD")
+plot!(x1eps, x2eps, seriestype = :scatter, label="GD")
 savefig("start_from_0p6.png")
 
 # xlims!(-.65, 1.05)
 # ylims!(-.45, 1.05)
 
-x1s, x2s, x2eps = main(1,1)  # 1,1 does not converge !? nor 0.7,0.7 and higher tenths places
+x1s, x2s, x1eps, x2eps = main(1,1)  # 1,1 does not converge !? nor 0.7,0.7 and higher tenths places
 plot(x1s, x2s, seriestype = :scatter, label="PS")
-plot!(x1s, x2eps, seriestype = :scatter, label="GD")
+plot!(x1eps, x2eps, seriestype = :scatter, label="GD")
 savefig("start_from_1.png")
 
 
